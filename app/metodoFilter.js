a85427124a87e52cbac23a89c5ff3e85c3fbd09c
Eduardo Sousa Horvate <eduardo2491998@gmail.com>
2026-03-25 04:59:35 -0300

    refactor: improve filtering logic and function naming in metodoFilter.js
    - renamed variables for better clarity
    - extracted available books logic
    - improved code readability and structure

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,23 +1,30 @@
 const botoes = document.querySelectorAll('.btn');
-botoes.forEach(btn => btn.addEventListener("click", filtrarLivros));
+botoes.forEach(btn => btn.addEventListener("click", handleFilterClick));
 
-function filtrarLivros() {
-    const elementobtn = document.getElementById(this.id);
-    const categoria = elementobtn.value;
-    const livrosFiltrados = categoria == "disponivel"
-        ? livros.filter(livro => livro.quantidade > 0)
-        : livros.filter(livro => livro.categoria === categoria);
-    exibirLivrosNaTela(livrosFiltrados);
-    if (categoria == "disponivel") {
-        const valorTotal = calcularValorTotalDeLivrosFiltrados(livrosFiltrados);
-        exibirValorTotalDosLivrosNaTela(valorTotal);
+function handleFilterClick() {
+    const selectedCategory = this.value;
+    const filteredBooks = filterBooksByCategory(selectedCategory, livros);
+    exibirLivrosNaTela(filteredBooks);
+    handleAvailableBooks(selectedCategory, filteredBooks);
+}
+
+function exibirValorTotalDosLivrosNaTela(totalValue) {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+   <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${totalValue}</span></p>
+    </div>
+  `;
+}
+
+const handleAvailableBooks = (selectedCategory, livrosFiltrados) => {
+    if (selectedCategory === "disponivel") {
+        const totalValue = calcularValorTotalDeLivrosFiltrados(livrosFiltrados);
+        exibirValorTotalDosLivrosNaTela(totalValue);
     }
 }
 
-function exibirValorTotalDosLivrosNaTela(valorTotal) {
-    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
-   <div class="livros__disponiveis">
-      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
-    </div>
-  `;
+const filterBooksByCategory = (selectedCategory, livros) => {
+    return selectedCategory === "disponivel"
+        ? livros.filter(livro => livro.quantidade > 0)
+        : livros.filter(livro => livro.categoria === selectedCategory);
 }
